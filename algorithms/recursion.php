52d9219999427de155aa2dcaf281a4de44a1e2ab
Герман Болдырев <boldyrev_german@mail.ru>
2023-03-28 18:04:29 +0300

    s

--- algorithms/recursion.php
+++ algorithms/recursion.php
@@ -1,10 +1,17 @@
 <?php
 
+// Число для поиска фокториала
+$factorialOf = 5;
+
+// Функция для поиска факториала числа, которое мы в нее передаем
 function factorial($n)
 {
     if ($n == 1) {
         return 1;
     } else {
-        retrun $fa
+       return 0;
     }
 }
+
+// Выводим факториал числа
+echo factorial($factorialOf);
