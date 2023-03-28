549ffd59cf48973ce161db7ede54524846e0dc53
Герман Болдырев <boldyrev_german@mail.ru>
2023-03-28 17:58:32 +0300

    a

---
+++ algorithms/quickSort.php
@@ -0,0 +1,13 @@
+<?php
+
+// Исходный массив который надо отсортировать
+$arrayToSort = [14, 2, 4, 1, 3, 7, 13, 6, 8, 11, 9, 10, 12, 5, 15];
+
+function quickSort($array)
+{
+    
+}
+
+// Выводим отсортированный массив
+echo '<pre>';
+print_r(selectionSort($arrayToSort));
